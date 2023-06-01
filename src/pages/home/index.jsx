f66e1337e23339ab78f65c0bacad41ddd42a34bc
RohendoJ <rohendoj3134.github@gmail.com>
2023-06-01 22:12:59 +0700

    change interval 5 sec

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -27,7 +27,7 @@ const Home = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       fetchStudents();
-    }, 1000);
+    }, 5000);
 
     return () => clearInterval(interval);
   }, []);
